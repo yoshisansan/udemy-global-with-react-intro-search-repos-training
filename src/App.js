31c20b4cbfe0d13c0bd5d545404f86a42090df33
Akifumi <kusacurrency@gmail.com>
2019-05-10 17:12:48 +0700

    show search results

--- src/App.js
+++ src/App.js
@@ -50,7 +50,23 @@ class App extends React.Component {
                 const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
                 const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
                 console.log(data.search)
-                return <h2>{title}</h2>
+                return (
+                  <React.Fragment>
+                    <h2>{title}</h2>
+                    <ul>
+                      {
+                        search.edges.map(edge=>{
+                          const node = edge.node
+                          return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank">{node.name}</a>
+                          </li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </React.Fragment>
+                )
               }
             }
           </Query>

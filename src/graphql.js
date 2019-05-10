eda2ed1043e2ecba69f5a3e8bfb980c534209f88
Akifumi <kusacurrency@gmail.com>
2019-05-10 16:25:53 +0700

    apply query and variables

--- src/graphql.js
+++ src/graphql.js
@@ -1,10 +1,38 @@
 import gql from 'graphql-tag'
 
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`
+
 export const ME = gql`
-query me {
-  user(login: "yoshisansan"){
-    name
-		avatarUrl    
-   }
-}
+  query me {
+    user(login: "iteachonudemy") {
+      name
+      avatarUrl
+    }
+  }
 `
